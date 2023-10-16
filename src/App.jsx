c6bed400de4fe4f4772cf99bedb689afbed34091
Tyler Aycock <tyler.joseph.aycock@gmail.com>
2023-10-16 13:54:13 -0700

    initial commit and header section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import link from "./assets/images/icon-linkedin.svg";
+import git from "./assets/images/icon-github.svg";
+import circle from "./assets/images/pattern-circle.svg";
+import rings from "./assets/images/pattern-rings.svg";
+
+
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="layout">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
